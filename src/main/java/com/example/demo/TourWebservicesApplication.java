aa1661f20cd551904c796e6d3b074f5a44dcb98c
Purva Ughade <purvaughade58@gmail.com>
2025-10-15 10:54:35 +0530

    Tour=webservices (Pandharpr) os added

---
+++ src/main/java/com/example/demo/TourWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourWebservicesApplication.class, args);
+	}
+
+}
